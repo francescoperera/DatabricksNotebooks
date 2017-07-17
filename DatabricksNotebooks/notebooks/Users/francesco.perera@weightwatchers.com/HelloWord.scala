f6a539dd0b7d89fd186a8563715066f3e4a06ad1
Francesco Perera <francesco.perera@nyfpereramac.na.weightwatchers.net;online.weightwatchers.net;as.weightwatchers.net;weightwatchers.net;eu.weightwatchers.net;weightwatchers.prod;wwi.lan;apac.weightwatchers.prod;network.na.weightwatchers.net>
2017-07-17 17:28:19 -0400

    added a comment in Sublime

---
+++ DatabricksNotebooks/notebooks/Users/francesco.perera@weightwatchers.com/HelloWord.scala
@@ -0,0 +1,18 @@
+// Databricks notebook source
+val hi = "Hello "
+val defaultName = "World"
+def personalizedHi( name: Option[String]): Unit = {
+  name match {
+    case Some(n) => println(hi + n)
+    case None => println(hi + defaultName)
+  }
+}
+
+// COMMAND ----------
+
+personalizedHi(None)
+personalizedHi(Some("John"))
+//adding a comment
+
+// COMMAND ----------
+
