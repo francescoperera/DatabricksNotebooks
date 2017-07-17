33050b9409f5bbe7cbbba0a5e482dbbf7527ad67
Francesco Perera <fperera29@gmail.com>
2017-07-17 17:23:11 -0400

    added old personalized method

--- notebooks/Users/francesco.perera@weightwatchers.com/HelloWord.scala
+++ notebooks/Users/francesco.perera@weightwatchers.com/HelloWord.scala
@@ -1,6 +1,12 @@
 // Databricks notebook source
-val hi = "Hello World"
-println (hi)
+val hi = "Hello "
+val defaultName = "World"
+def personalizedHi( name: Option[String]): Unit = {
+  name match {
+    case Some(n) => println(hi + n)
+    case None => println(hi + defaultName)
+  }
+}
 
 // COMMAND ----------
 
